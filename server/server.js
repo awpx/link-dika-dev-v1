908795beaae1dbc6caa953a662c7eb011d9305e9
awp <awpxwork@gmail.com>
2020-10-03 09:01:00 +0700

    prepare for deploy

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 require('dotenv').config({ path: './server/config/config.env' })
@@ -15,6 +16,22 @@ connectDB()
 //routes
 app.use('/', require('./routes/index'))
 app.use('/url/api/v1/shorten', require('./routes/url'))
+
+//static
+//production build
+if(process.env.NODE_ENV === 'production') {
+  const root = path.join(__dirname, '../client', 'build')
+  console.log(root)
+
+  app.use(express.static(root))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html')))
+  console.log(path.resolve())
+} else {
+  app.get('/', (req, res) => {
+    res.send('api services running')
+  })
+}
 
 //morgan
 if (process.env.NODE_ENV === 'development') {
